2432fb54c7f1a5c667f166ca29ade8d2e45dec5d
Rodrigo Lima <rodrigo.sad.lima@gmail.com>
2020-06-18 15:51:03 -0300

    added debounce back, except first render

--- src/hooks/useSizeOf.js
+++ src/hooks/useSizeOf.js
@@ -1,14 +1,33 @@
 import { useState, useLayoutEffect, useCallback } from "react";
 
+const debounce = (func, delay) => {
+  let debounceTimer;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+  };
+};
+
 export default function (div) {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-  const updateSize = useCallback(() => {
-    const [width, height] = [div.current.clientWidth, div.current.clientHeight];
-    setDimensions({ width, height });
-  }, [div]);
+
+  const updateSize = useCallback(
+    debounce(() => {
+      const [width, height] = [
+        div.current.clientWidth,
+        div.current.clientHeight,
+      ];
+      setDimensions({ width, height });
+    }, 500),
+
+    [div]
+  );
 
   useLayoutEffect(() => {
-    updateSize();
+    const [width, height] = [div.current.clientWidth, div.current.clientHeight];
+    setDimensions({ width, height });
     window.addEventListener("resize", updateSize);
     return () => window.removeEventListener("resize", updateSize);
   }, [updateSize]);
