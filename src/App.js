4af69c05a49f5b722053bc2285a33e81c0979d85
Rodrigo Lima <rodrigo.sad.lima@gmail.com>
2020-06-18 14:07:12 -0300

    centered CSS

--- src/App.js
+++ src/App.js
@@ -3,27 +3,25 @@ import Loader from "react-loader-spinner";
 import useSizeOf from "./hooks/useSizeOf";
 import useCSV from "./hooks/useCSV";
 
+import Circles from "./components/Circles";
+
 import "./App.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function App() {
   const div = useRef();
   const dimensions = useSizeOf(div);
-
   const [data, fetching] = useCSV(
     "https://raw.githubusercontent.com/curran/data/gh-pages/uci_ml/auto-mpg/auto-mpg.csv"
   );
+
   return (
     <div className="App" ref={div}>
-      <h1>Hello World</h1>
       {fetching ? (
         <Loader type="Circles" />
       ) : (
         <Circles data={data} dimensions={dimensions}></Circles>
       )}
-      <h1>
-        {dimensions.width},{dimensions.height}
-      </h1>
     </div>
   );
 }
--- src/components/Circles.js
+++ src/components/Circles.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function () {
-  return <svg></svg>;
+export default function ({ data, dimensions }) {
+  const { width, height } = dimensions;
+  return (
+    <svg width={width} height={height}>
+      <rect width={width} height={height}></rect>
+    </svg>
+  );
 }
